9b70e9f34e0727ade40564fc94a86e595a198297
Yeondam Park <giyen9088@gmail.com>
2016-11-02 23:02:08 -0700

    Setting webpack and integrated to gulp task

---
+++ gulpfile.babel.js
@@ -0,0 +1,54 @@
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import eslint from 'gulp-eslint';
+import del from 'del';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
+
+const paths = {
+  allSrcJs: 'src/**/*.js',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
+  gulpFile: 'gulpfile.babel.js',
+  webpackFile: 'webpack.config.babel.js',
+  libDir: 'lib',
+  distDir: 'dist',
+  clientBundle: 'dist/client-bundle.js?(.map)',
+};
+
+gulp.task('lint', () =>
+  gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+    paths.webpackFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+);
+
+gulp.task('clean', () => del([
+  paths.libDir,
+  paths.clientBundle,
+]));
+
+gulp.task('build', ['lint', 'clean'], () =>
+  gulp.src(paths.allSrcJs)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir))
+);
+
+gulp.task('main', ['lint', 'clean'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir))
+);
+
+gulp.task('watch', () => {
+  gulp.watch(paths.allSrcJs, ['main']);
+});
+
+gulp.task('default', ['watch', 'main']);
---
+++ src/server/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+
+import Dog from '../shared/dog';
+
+const Toby = new Dog('Toby');
+
+console.log(Toby.bark());
+
